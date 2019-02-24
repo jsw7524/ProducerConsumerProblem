1921853149cf989810cd659bef25ac716c6148b1
jsw7524 <jsw7524@gmail.com>
2019-02-25 00:36:25 +0800

    ProducerConsumer

--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -2,20 +2,92 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProducerConsumer
 {
+    class ProducerConsumer
+    {
+
+        private Semaphore semaphoreSpace;
+
+        private Semaphore semaphoreGoods;
+
+        public Stack<int> stack;
+
+
+        private Mutex mutexProducer;
+
+        private Mutex mutexConsumer;
+
+        private int buffer;
+
+        public ProducerConsumer()
+        {
+            buffer = 0;
+            semaphoreSpace = new Semaphore(10, 10);
+            semaphoreGoods = new Semaphore(0, 10);
+
+            mutexProducer = new Mutex(false);
+            mutexConsumer = new Mutex(false);
+            stack = new Stack<int>(10);
+        }
+
+        public async Task Producer()
+        {
+            await Task.Run(() =>
+            {
+                while (true) //for (int i=0;i<10;i++)
+                {
+                    semaphoreSpace.WaitOne();
+                    mutexProducer.WaitOne();
+                    stack.Push(Thread.CurrentThread.ManagedThreadId);
+                    mutexProducer.ReleaseMutex();
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} add 1 to buffer. buffer:{buffer}");
+                    semaphoreGoods.Release();
+
+                }
+            });
+
+        }
+        public async Task Consumer()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    semaphoreGoods.WaitOne();
+                    mutexConsumer.WaitOne();
+                    var v = stack.Pop();
+                    Console.WriteLine(v);
+                    mutexConsumer.ReleaseMutex();
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} add 1 to buffer. buffer:{buffer}");
+                    semaphoreSpace.Release();
+                }
+
+            });
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var pc = new ProducerConsumer();
+            for (int i = 1; i <= 10; i++)
+            {
+                pc.Producer();
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                pc.Consumer();
+            }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            var yyy = pc.stack;
+            Console.WriteLine("YYYYYYYYYYYYY");
+            Console.ReadKey();
         }
     }
 }
